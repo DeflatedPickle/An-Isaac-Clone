1a6985cde94dbb9c0e131bdc4a411b850b49acc4
DeflatedPickle <DeflatedPickle@gmail.com>
2018-02-21 07:51:11 +0000

    Added a movement script

---
+++ AnIsaacClone/Assets/Scripts/Player/Move.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class Move : MonoBehaviour {
+	[Range(1, 30)]
+	public float MoveForce = 18f;
+	[Range(1, 20)]
+	public float MaxSpeed = 10f;
+
+	private Rigidbody2D _rigidbody2D;
+
+	private void Awake() {
+		_rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) {
+			var horizontal = Input.GetAxis("Horizontal");
+
+			if (horizontal * _rigidbody2D.velocity.x < MaxSpeed) {
+				_rigidbody2D.AddForce(Vector2.right * horizontal * MoveForce);
+			}
+
+			if (Mathf.Abs(_rigidbody2D.velocity.x) > MaxSpeed) {
+				_rigidbody2D.velocity = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x) * MaxSpeed, _rigidbody2D.velocity.y);
+			}
+		}
+		
+		if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)) {
+			var vertical = Input.GetAxis("Vertical");
+
+			if (vertical * _rigidbody2D.velocity.y < MaxSpeed) {
+				_rigidbody2D.AddForce(Vector2.up * vertical * MoveForce);
+			}
+
+			if (Mathf.Abs(_rigidbody2D.velocity.y) > MaxSpeed) {
+				_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, Mathf.Sign(_rigidbody2D.velocity.y) * MaxSpeed);
+			}
+		}
+	}
+}
